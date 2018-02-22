d143cca0a40e0f4a7be43f06840f7f57384ba478
Reginald Ferland <rferlan@g.clemson.edu>
2018-02-22 12:35:51 -0500

    updated footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
-<footer>
+<footer class="page-footer">
 <hr>
 
-<p>MeTube is an Open Source Project, here is the <a href="https://www.github.com/reginaldFerland">Source Code</a>.</p>
+<p>MeTube is an Open Source Project, you can read the source code <a href="https://www.github.com/reginaldFerland/MeTube">here</a>.</p>
 
 </footer>
