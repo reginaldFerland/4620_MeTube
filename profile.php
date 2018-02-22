443190e322fb4b40ebf17070a163d623116379da
Reginald Ferland <rferlan@g.clemson.edu>
2018-02-22 14:37:27 -0500

    created empty profile page

---
+++ profile.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<!-- Use php to create title -->
+<title> Profile </title>
+
+<!-- Add css sheet -->
+
+</head>
+<body>
+<!-- Header -->
+<?php include('header.php'); ?>
+
+placeholder
+
+<!-- Footer -->
+<?php include('footer.php'); ?>
+</body>
+</html>
