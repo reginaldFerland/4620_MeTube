36f2e225824de9b9d2cfe1c9baf7250e291fc0ea
Reginald Ferland <rferlan@g.clemson.edu>
2018-04-14 13:41:19 -0400

    formmated register.php

--- register.php
+++ register.php
@@ -17,56 +17,58 @@ session_start();
 include_once "function.php";
 
 if(isset($_POST['submit'])) {
-	if( $_POST['passowrd1'] != $_POST['passowrd2']) {
-		$register_error = "Passwords don't match. Try again?";
-	}
-	else {
-                $checkEmail = validate_email($_POST['email']);
-                if($checkEmail == 1){
-                
-		$check = user_exist_check($_POST['username'], $_POST['passowrd1'], $_POST['email']);	
-		if($check == 1){
-			//echo "Register succeeds";
-			$_SESSION['username']=$_POST['username'];
-			header('Location: .');
-		}
-		else if($check == 2){
-			$register_error = "Username already exists. Please use a different username.";
-		}
-                else if($check == 3){
-                        $register_error = "Email already exists. Please use a different email.";
-                }
-                }
-                else{
-                    // Bad email
-                    $register_error = "Invalid Email";
-               }
+    if( $_POST['passowrd1'] != $_POST['passowrd2']) {
+        $register_error = "Passwords don't match. Try again?";
+    }
+    else 
+    {
+        $checkEmail = validate_email($_POST['email']);
+        if($checkEmail == 1){
+            $check = user_exist_check($_POST['username'], $_POST['passowrd1'], $_POST['email']);    
+            if($check == 1){
+                //echo "Register succeeds";
+                $_SESSION['username']=$_POST['username'];
+                header('Location: .');
+            }
+            else if($check == 2){
+                $register_error = "Username already exists. Please use a different username.";
+            }
+            else if($check == 3){
+                $register_error = "Email already exists. Please use a different email.";
+            }
+        }
+        else{
+            // Bad email
+            $register_error = "Invalid Email";
+        }
 
-	}
+    }
 }
 
 ?>
 <form action="register.php" method="post" style="max-width:200px; margin: 20px" class="mx-auto">
-  <div class="form-group">
-    <input type="text" name="username" placeholder="Username">
-  </div>
-  <div class="form-group">
-    <input type="text" name="email" placeholder="Email">
-  </div>
-  <div class="form-group">
-    <input  type="password" name="passowrd1" placeholder="Password">
-  </div>
-  <div class="form-group">
-    <input type="password" name="passowrd2" placeholder="Repeat Password">
-  </div>
-  <div class="form-group">
-    <input name="submit" type="submit" value="Submit">
-  </div>
+    <div class="form-group">
+        <input type="text" name="username" placeholder="Username">
+    </div>
+    <div class="form-group">
+        <input type="text" name="email" placeholder="Email">
+    </div>
+    <div class="form-group">
+        <input  type="password" name="passowrd1" placeholder="Password">
+    </div>
+    <div class="form-group">
+        <input type="password" name="passowrd2" placeholder="Repeat Password">
+    </div>
+    <div class="form-group">
+        <input name="submit" type="submit" value="Submit">
+    </div>
 </form>
 
 <?php
-  if(isset($register_error))
-   {  echo "<div id='passwd_result'> register_error:".$register_error."</div>";}
+    if(isset($register_error))
+    {  
+        echo "<div id='passwd_result'> register_error:".$register_error."</div>";
+    }
 ?>
 
 <?php
