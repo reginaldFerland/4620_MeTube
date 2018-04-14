fa82d6e7e5b253845b17b103144e16e88ea3add3
Reginald Ferland <rferlan@g.clemson.edu>
2018-04-14 13:36:39 -0400

    reformatted media_upload_process.php

--- media_upload_process.php
+++ media_upload_process.php
@@ -14,51 +14,53 @@ $username=$_SESSION['username'];
 
 //Create Directory if doesn't exist
 if(!file_exists('uploads/'))
-	mkdir('uploads/', 0757);
+    mkdir('uploads/', 0757);
 $dirfile = 'uploads/'.$username.'/';
 if(!file_exists($dirfile))
-	mkdir($dirfile,0755);
-	chmod( $dirfile,0755);
-	if($_FILES["file"]["error"] > 0 )
-	{ 	$result=$_FILES["file"]["error"];} //error from 1-4
-	else
-	{
-		$upfile = $dirfile.urlencode($_FILES["file"]["name"]);
-	  
-                $filename = $_FILES["file"]["name"];
-                $uploads = "select uploads from account where username = '" . $username . "'";
-                $results = mysql_query($uploads);
-		$results_row = mysql_fetch_assoc($results);
-                $file_basename = $results_row["uploads"];
-		$file_ext = substr($filename, strripos($filename, '.')); 
-                $upfile = $dirfile.$file_basename . $file_ext;
-                $update = "UPDATE account set uploads = uploads + 1 where username = '" . $username . "'";
-                $date = date('c');
-                mysql_query($update);
-			if(is_uploaded_file($_FILES["file"]["tmp_name"]))
-			{
-				if(!move_uploaded_file($_FILES["file"]["tmp_name"],$upfile))
-				{
-					$result="6"; //Failed to move file from temporary directory
-				}
-				else /*Successfully upload file*/
-				{
-					//insert into media table
-					$insert = "insert into media(mediaid, filename,username,type, path,upload_time)".
-	  "values(NULL,'". urlencode($_FILES["file"]["name"])."','$username','".$_FILES["file"]["type"]."', '$upfile','$date')";
-					$queryresult = mysql_query($insert)
-						  or die("Insert into Media error in media_upload_process.php " .mysql_error());
-					$result="0";
-					chmod($upfile, 0644);
-				}
-			}
-			else  
-			{
-					$result="7"; //upload file failed
-			}
-	}
-	
-	//You can process the error code of the $result here.
+    mkdir($dirfile,0755);
+    chmod( $dirfile,0755);
+    if($_FILES["file"]["error"] > 0 )
+    {   
+        $result=$_FILES["file"]["error"];
+    } //error from 1-4
+    else
+    {
+        $upfile = $dirfile.urlencode($_FILES["file"]["name"]);
+      
+        $filename = $_FILES["file"]["name"];
+        $uploads = "select uploads from account where username = '" . $username . "'";
+        $results = mysql_query($uploads);
+        $results_row = mysql_fetch_assoc($results);
+        $file_basename = $results_row["uploads"];
+        $file_ext = substr($filename, strripos($filename, '.')); 
+        $upfile = $dirfile.$file_basename . $file_ext;
+        $update = "UPDATE account set uploads = uploads + 1 where username = '" . $username . "'";
+        $date = date('c');
+        mysql_query($update);
+        if(is_uploaded_file($_FILES["file"]["tmp_name"]))
+        {
+            if(!move_uploaded_file($_FILES["file"]["tmp_name"],$upfile))
+            {
+                $result="6"; //Failed to move file from temporary directory
+            }
+            else /*Successfully upload file*/
+            {
+                //insert into media table
+                $insert = "insert into media(mediaid, filename,username,type, path,upload_time)".
+                "values(NULL,'". urlencode($_FILES["file"]["name"])."','$username','".$_FILES["file"]["type"]."', '$upfile','$date')";
+                $queryresult = mysql_query($insert)
+                or die("Insert into Media error in media_upload_process.php " .mysql_error());
+                $result="0";
+                chmod($upfile, 0644);
+                }
+        }
+        else  
+        {
+            $result="7"; //upload file failed
+        }
+    }
+    
+    //You can process the error code of the $result here.
 ?>
 
 <meta http-equiv="refresh" content="0;url=.?result=<?php echo $result;?>">
