8ef783a3d5f986baf8cc45ac6d647fe1a0bfc9d6
Reginald Ferland <rferlan@g.clemson.edu>
2018-04-14 13:26:34 -0400

    converted header.php from 2 spaces to 4

--- header.php
+++ header.php
@@ -2,40 +2,40 @@
 
 <!-- Nav bar -->
 <nav class="navbar navbar-expand navbar-inverse bg-dark">
-  <!-- Logo -->
-  <a class="navbar-brand" href="./">METUBE</a>
+    <!-- Logo -->
+    <a class="navbar-brand" href="./">METUBE</a>
 
-  <!-- Search Bar -->
-  <form class="form-inline" action="./search.php" method="POST">
-    <input class="form-control mr-sm-2" name="search" type="text" placeholder="Search">
-    <button class="btn btn-success" type="submit">Search</button>
-  </form>
+    <!-- Search Bar -->
+    <form class="form-inline" action="./search.php" method="POST">
+        <input class="form-control mr-sm-2" name="search" type="text" placeholder="Search">
+        <button class="btn btn-success" type="submit">Search</button>
+    </form>
 
-  <!-- Determine if logged in -->
-  <?php
-  if(isset($_SESSION['username'])) { ?>
-    <!-- Profile and Log out -->
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-          <a class="nav-link" href="media_upload.php">Upload File</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="./profile.php?username=<?php echo $_SESSION["username"];?>">Profile</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="./logout.php">Log Out</a>
-      </li>
-    </ul>
-  <?php } else { ?>
-    <!-- Log in or Register -->
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="./login.php">Login</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="./register.php">Register</a>
-      </li>
-    </ul>
+    <!-- Determine if logged in -->
+    <?php
+    if(isset($_SESSION['username'])) { ?>
+        <!-- Profile and Log out -->
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="media_upload.php">Upload File</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./profile.php?username=<?php echo $_SESSION["username"];?>">Profile</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./logout.php">Log Out</a>
+            </li>
+        </ul>
+    <?php } else { ?>
+        <!-- Log in or Register -->
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="./login.php">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="./register.php">Register</a>
+            </li>
+        </ul>
    <?php  } ?>
 
 </nav>
