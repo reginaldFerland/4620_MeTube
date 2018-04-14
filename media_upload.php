845497ef9ae537dda78630355888e762c50021d6
Reginald Ferland <rferlan@g.clemson.edu>
2018-04-14 13:33:41 -0400

    reformatted media_upload.php

--- media_upload.php
+++ media_upload.php
@@ -21,16 +21,16 @@ session_start();
 
 <form method="post" action="media_upload_process.php" enctype="multipart/form-data" >
  
-  <p style="margin:0; padding:0">
-  <input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
-   Add a Media: <label style="color:#663399"><em> (Each file limit 10M)</em></label><br/>
-   <input  name="file" type="file" size="50" />
+    <p style="margin:0; padding:0">
+    <input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
+        Add a Media: <label style="color:#663399"><em> (Each file limit 10M)</em></label><br/>
+    <input  name="file" type="file" size="50" />
   
-	<input value="Upload" name="submit" type="submit" />
-  </p>
+    <input value="Upload" name="submit" type="submit" />
+    </p>
  
                 
- </form>
+</form>
 
 <?php
     include('footer.php');
