fe9b588eabc9cb23d342307dddb229fcb02ee3e1
Reginald Ferland <rferlan@g.clemson.edu>
2018-04-14 13:29:40 -0400

    coverted login.php to unified formatting

--- login.php
+++ login.php
@@ -18,43 +18,44 @@ session_start();
 include_once "function.php";
 
 if(isset($_POST['submit'])) {
-		if($_POST['username'] == "" || $_POST['password'] == "") {
-			$login_error = "One or more fields are missing.";
-		}
-		else {
-			$check = user_pass_check($_POST['username'],$_POST['password']); // Call functions from function.php
-			if($check == 1) {
-				$login_error = "User ".$_POST['username']." not found.";
-			}
-			elseif($check==2) {
-				$login_error = "Incorrect password.";
-			}
-			else if($check==0){
-				$_SESSION['username']=$_POST['username']; //Set the $_SESSION['username']
-				header('Location: .');
-			}		
-		}
+    if($_POST['username'] == "" || $_POST['password'] == "") {
+        $login_error = "One or more fields are missing.";
+    }
+    else {
+        $check = user_pass_check($_POST['username'],$_POST['password']); // Call functions from function.php
+        if($check == 1) {
+            $login_error = "User ".$_POST['username']." not found.";
+        }
+        elseif($check==2) {
+            $login_error = "Incorrect password.";
+        }
+        else if($check==0){
+            $_SESSION['username']=$_POST['username']; //Set the $_SESSION['username']
+            header('Location: .');
+        }       
+    }
 }
 
 
  
 ?>
 <form method="post" action="<?php echo "login.php"; ?>" style="max-width: 500px; margin: 20px auto">
-  <div class="form-group">
-    <input class="form-control" type="username" name="username" placeholder="Username">
-  </div>
-  <div class="form-group">
-    <input class="form-control" type="password" name="password" placeholder="Password">
-  </div>
-  <div class="form-group">
-    <input name="submit" type="submit" value="Login">
-  </div>
+    <div class="form-group">
+        <input class="form-control" type="username" name="username" placeholder="Username">
+    </div>
+    <div class="form-group">
+        <input class="form-control" type="password" name="password" placeholder="Password">
+    </div>
+    <div class="form-group">
+        <input name="submit" type="submit" value="Login">
+    </div>
 
 </form>
 
 <?php
-  if(isset($login_error))
-   {  echo "<div id='passwd_result'>".$login_error."</div>";}
+    if(isset($login_error)) {
+        echo "<div id='passwd_result'>".$login_error."</div>";
+    }
 ?>
 
 <?php
