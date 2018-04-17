839f07f2ffbcca44c16b9e5671366a9ad52fc807
Reginald Ferland <rferlan@g.clemson.edu>
2018-04-17 18:43:34 -0400

    fix

--- temp.php
+++ temp.php
@@ -1,6 +1,5 @@
 <?php
-
-chmod(./site 0644);
-chmod(./site/profile.png 0644);
-chmod(./site/thumbnail.png 0644);
+chmod("./site",0644);
+chmod("./site/profile.png",0644);
+chmod("./site/thumbnail.png",0644);
 ?>
